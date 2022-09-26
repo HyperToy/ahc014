372a2f9575a65b295f424a58532eadf1ce3542ae
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-09-26 23:41:32 +0900

    first commit

---
+++ a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+void input() {
+
+}
+
+void eval() {
+
+}
+
+void init() {
+
+}
+
+void simulate() {
+
+}
+
+void output() {
+
+}
+
+void solve() {
+    input();
+    init();
+    // simulate();
+    output();
+}
+
+int main(){
+    solve();
+} 
