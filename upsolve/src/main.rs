5bbf988a087a2626e662bd1d0e3462cab30826a8
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-10-12 20:14:43 +0900

    [update] rust: 入力を受け取って虚無を出力

--- upsolve/src/main.rs
+++ upsolve/src/main.rs
@@ -4,10 +4,18 @@ use proconio::*;
 
 fn main() {
     let input = read_input();
+    let output = solve(&input);
+    write_output(&output);
+}
+
+fn solve(input: &Input) -> Output {
+    let mut best = vec![];
+    best
 }
 
 type P = (i32, i32);
 type Rect = ([P; 4], usize);
+type Output = Vec<Rect>;
 
 struct Input {
     N: usize,
@@ -19,4 +27,18 @@ fn read_input() -> Input {
         ps: [(i32, i32); M],
     }
     Input { N, ps }
+}
+
+fn write_output(output: &Output) {
+    println!("{}", output.len());
+    for rect in output {
+        for i in 0..4 {
+            println!("{} {}", rect.0[i].0, rect.0[i].1);
+            if i == 3 {
+                println!();
+            } else {
+                println!(" ");
+            }
+        }
+    }
 }
